e84ca1409135261d591d2a9ca749086e66a43123
exthem <100224409+exthem@users.noreply.github.com>
2022-02-22 22:25:49 +0530

    Update exthem.js

--- exthem.js
+++ exthem.js
@@ -1,12 +1,1 @@
-//<![CDATA[
-$(document).ready(function()
-{
-var aa=$("#exthem").val();
-if (aa == null) {
-    window.location.href = "https://apktemplates.com/";
-};
- $("#exthem").attr("href","
-https://apktemplates.com/");
-
-});
-//]]>
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('$(f).e(d(){c 3=$("#2").b();a(3==9){8.7.1="0://6.5/"};$("#2").4("1","0:});',16,16,'https|href|exthem|aa|attr|com|apktemplates|location|window|null|if|val|var|function|ready|document'.split('|'),0,{}))
